a0c55d1c0679655dbdc3206602bc0da7735dd5b2
Kavaljeet Singh <748291@student.tor.loc>
2019-05-22 13:46:28 -0400

    Commit Message

---
+++ variables.js
@@ -0,0 +1,14 @@
+// Kavaljeet Singh
+/* c0748291
+csd 2214 section 1 */
+ 
+let FirstName = "Kaval";
+let LastName = "Singh";
+
+let a = 9;
+let b = 20;
+let c = a+b;
+
+
+alert(" Hello " + FirstName + " the answer is " + c);
+console.log("I made to end of program without any errors......");
